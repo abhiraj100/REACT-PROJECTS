1ef16eab3d795ea35271c15af3a96a2ace2311a0
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-10-03 21:36:05 +0530

    Handle multiple Fetch Requests

--- Countries-Information - React-Router-Dom/src/Components/CountryCard.jsx
+++ Countries-Information - React-Router-Dom/src/Components/CountryCard.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const CountryCard = ({ name, flag, population, region, capital }) => {
   return (
-    <Link className="country-card" to={`/country?name=${name}`}>
+    // <Link className="country-card" to={`/country?name=${name}`}>
+    <Link className="country-card" to={`/${name}`}>
     <img src={flag} alt={name + ' Flag'} />
     <div className="card-text">
       <h3 className="card-title">{name}</h3>
